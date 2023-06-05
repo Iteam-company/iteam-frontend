404823ec25b4283ca55f3c5716e36b7fadfb3381
Igor <sergienko339@gmail.com>
2023-06-05 17:15:19 +0300

    added last parts of nodemailer lost logic fix

--- src/axios.ts
+++ src/axios.ts
@@ -2,35 +2,44 @@ import axios from "axios";
 
 const APP_URL = process.env.REACT_APP_STRAPI_URL;
 
-const client = axios.create({ 
-    baseURL: APP_URL });
+const client = axios.create({
+    baseURL: APP_URL,
+});
 
 client.interceptors.request.use((config) => {
-    config.headers.Authorization = localStorage.getItem('token');
+    config.headers.Authorization = localStorage.getItem("token");
     return config;
 });
 
-client.interceptors.response.use((config) => {
-    return config;
-}, async (error) => {
-    const originalReq = error.config;
+client.interceptors.response.use(
+    (config) => {
+        return config;
+    },
+    async (error) => {
+        const originalReq = error.config;
 
-    if (error.response.status === 403 &&
-        error.config &&
-        !error.config._isRetry) {
+        if (
+            error.response.status === 403 &&
+            error.config &&
+            !error.config._isRetry
+        ) {
             originalReq._isRetry = true;
 
-            try {              
-                localStorage.setItem('token', `${process.env.REACT_APP_STRAPI_TOKEN}`);
+            try {
+                localStorage.setItem(
+                    "token",
+                    `${process.env.REACT_APP_STRAPI_TOKEN}`,
+                );
 
                 return client.request(originalReq);
-            }  catch (error) {
-				console.log('Not auth! Error:' + error);
-                localStorage.removeItem('token');
-			}
+            } catch (error) {
+                console.log("Not auth! Error:" + error);
+                localStorage.removeItem("token");
+            }
         }
 
         throw error;
-})
+    },
+);
 
 export default client;
