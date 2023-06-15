aabb77af170b7e57d67cce9c1afaa2c49ac50579
Maxim <gumeniukmaks0510@gmail.com>
2023-06-15 14:05:42 +0300

    add link on contact us

--- src/anatomic/molecules/Info/Info.tsx
+++ src/anatomic/molecules/Info/Info.tsx
@@ -4,6 +4,7 @@ import { COLORS } from "@/lib/theme/color";
 import React, { FC } from "react";
 import { Flex, Icon } from "./styled";
 import { getStrapiImage } from "@/hooks/useStrapiContentData";
+import Link from "next/link";
 
 interface Props {
     title: string;
@@ -14,7 +15,9 @@ interface contactInfoI {
     title: string;
     text: string;
     icon: any;
+    linkToContactInfo: string;
 }
+
 export const Info: FC<Props> = ({ title, description, contactInfo }) => {
     return (
         <Flex gap="35px" p="20px" w="40%">
@@ -29,26 +32,35 @@ export const Info: FC<Props> = ({ title, description, contactInfo }) => {
                 <Text color={COLORS.warmGray} size={TEXT_SIZES.small.xl}>
                     {description}
                 </Text>
-                {contactInfo.map((item) => (
-                    <FlexRow alignItems="center" gap="10px" key={item.title}>
-                        <Icon
-                            src={getStrapiImage(item.icon.data.attributes.url)}
-                        />
-                        <FlexColumn>
-                            <Text
-                                size={TEXT_SIZES.small.xl}
-                                color={COLORS.black}
-                            >
-                                {item.title}
-                            </Text>
-                            <Text
-                                size={TEXT_SIZES.small.xl}
-                                color={COLORS.black}
-                            >
-                                {item.text}
-                            </Text>
-                        </FlexColumn>
-                    </FlexRow>
+                {contactInfo.map((item, index: number) => (
+                    <Link
+                        key={item.title}
+                        href={item.linkToContactInfo}
+                        target={index === 2 ? "_blank" : ""}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <FlexRow alignItems="center" gap="10px">
+                            <Icon
+                                src={getStrapiImage(
+                                    item.icon.data.attributes.url,
+                                )}
+                            />
+                            <FlexColumn>
+                                <Text
+                                    size={TEXT_SIZES.small.xl}
+                                    color={COLORS.black}
+                                >
+                                    {item.title}
+                                </Text>
+                                <Text
+                                    size={TEXT_SIZES.small.xl}
+                                    color={COLORS.black}
+                                >
+                                    {item.text}
+                                </Text>
+                            </FlexColumn>
+                        </FlexRow>
+                    </Link>
                 ))}
             </FlexColumn>
         </Flex>
