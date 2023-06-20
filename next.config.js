05e02efaf48906232e6c0f17d61b68a8596fb9e4
Max <gumeniukmaks0510@gmail.com>
2023-06-20 14:10:26 +0300

    fix image

--- next.config.js
+++ next.config.js
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            port: '',
-            pathname: '/iteam-cloud/image/upload/**',
-          },
-        ],
-      },
-    env: {
-        REACT_APP_SMTP_HOST: process.env.SMTP_HOST,
-        REACT_APP_SMTP_PORT: process.env.REACT_APP_SMTP_PORT,
-        REACT_APP_SMTP_USER: process.env.REACT_APP_SMTP_USER,
-        REACT_APP_SMTP_PASSWORD: process.env.REACT_APP_SMTP_PASSWORD,
-        REACT_APP_STRAPI_URL: process.env.REACT_APP_STRAPI_URL,
-        REACT_APP_STRAPI_CONTENT_URL: process.env.REACT_APP_STRAPI_CONTENT_URL,
-        REACT_APP_DEPLOY_HOST: process.env.REACT_APP_DEPLOY_HOST,
-        REACT_APP_LOCAL_HOST: process.env.REACT_APP_LOCAL_HOST
-    },
-};
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/iteam-cloud/image/upload/**',
+      },
+    ],
+  },
+  env: {
+      NEXT_PUBLIC_ENV: 'PRODUCTION',
+      REACT_APP_SMTP_HOST: process.env.SMTP_HOST,
+      REACT_APP_SMTP_PORT: process.env.REACT_APP_SMTP_PORT,
+      REACT_APP_SMTP_USER: process.env.REACT_APP_SMTP_USER,
+      REACT_APP_SMTP_PASSWORD: process.env.REACT_APP_SMTP_PASSWORD,
+      REACT_APP_STRAPI_URL: process.env.REACT_APP_STRAPI_URL,
+      REACT_APP_STRAPI_CONTENT_URL: process.env.REACT_APP_STRAPI_CONTENT_URL,
+      REACT_APP_DEPLOY_HOST: process.env.REACT_APP_DEPLOY_HOST,
+      REACT_APP_LOCAL_HOST: process.env.REACT_APP_LOCAL_HOST
+  },
+})
--- src/anatomic/organisms/ExploreWithIteam/styled.ts
+++ src/anatomic/organisms/ExploreWithIteam/styled.ts
@@ -11,29 +11,28 @@ interface GridBoxProps {
 }
 
 interface SpotProps {
-  bg?: string;
+    bg?: string;
 }
 
 export const GridBox = styled.div<GridBoxProps>`
     width: 100%;
     display: grid;
-    grid-template-columns: ${props => props.gridTemplateColumn|| '1fr'};
-    grid-template-rows: ${props => props.gridTemplateRow || '1fr'};
-    grid-column-gap: ${props => props.gridColumnGap || '0'};
-    grid-row-gap: ${props => props.gridRowGap || '0'};
+    grid-template-columns: ${(props) => props.gridTemplateColumn || "1fr"};
+    grid-template-rows: ${(props) => props.gridTemplateRow || "1fr"};
+    grid-column-gap: ${(props) => props.gridColumnGap || "0"};
+    grid-row-gap: ${(props) => props.gridRowGap || "0"};
 
-
-    @media(max-width: 992px) {
+    @media (max-width: 992px) {
         grid-template-columns: 1fr;
         grid-template-rows: repeat(8, 325px);
         padding-bottom: 20px;
-      }
-      @media(max-width: 600px) {
+    }
+    @media (max-width: 600px) {
         grid-template-columns: 1fr;
-        grid-template-rows: repeat(8, 225px);
+        grid-template-rows: repeat(8, 180px);
         padding-bottom: 20px;
-      }
-`
+    }
+`;
 
 interface BoxProps {
     bg?: string;
@@ -41,101 +40,99 @@ interface BoxProps {
 }
 
 interface BoxForTextSpotProps {
-  t?: string;
-  l?: string;
+    t?: string;
+    l?: string;
 }
 
 export const Box = styled.div<BoxProps>`
-  position: relative;
-  border-radius: 8px;
-  background: ${props => props.backgroundImage || 'none'};
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-position: center;
-  overflow: hidden;
-
-  &::before {
-    background: ${props => props.bg || 'none'};
+    position: relative;
     border-radius: 8px;
-    content: '';
-    display: block;
-    height: 100%;
-    position: absolute;
-    width: 100%;
-    z-index: 1;
+    background: ${(props) => props.backgroundImage || "none"};
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-position: center;
     overflow: hidden;
-   
-  }
 
-  &:hover::before {
-    opacity: 0;
-    transition: opacity 1s ease-out;
-  }
+    &::before {
+        background: ${(props) => props.bg || "none"};
+        border-radius: 8px;
+        content: "";
+        display: block;
+        height: 100%;
+        position: absolute;
+        width: 100%;
+        z-index: 1;
+        overflow: hidden;
+    }
 
-`
+    &:hover::before {
+        opacity: 0;
+        transition: opacity 1s ease-out;
+    }
+`;
 
 export const BoxForText = styled.div`
-  margin-top: 28px;
-  padding-left: 24px;
-  position: relative;
-  z-index: 3;
-`
+    margin-top: 28px;
+    padding-left: 24px;
+    position: relative;
+    z-index: 3;
+`;
 
 export const BoxForPreview = styled.div`
-  position: absolute;
-  z-index: 2;
-  bottom: 10px;
-  right: 15px;
+    position: absolute;
+    z-index: 2;
+    bottom: 10px;
+    right: 15px;
 
-  @media (max-width: 600px) {
-    width: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-`
+    @media (max-width: 600px) {
+        width: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+`;
 
 export const BoxForSpot = styled.div<BoxForTextSpotProps>`
-  background: rgba(241, 241, 241, 1);
-  filter: blur(100px);
-  width: 50%;
-  height: 50%;
-  position: absolute;
-  top: ${props => props.t || '30%'};
-  left: ${props => props.l || '30%'};
-  z-index: 2;
-`
+    background: rgba(241, 241, 241, 1);
+    filter: blur(100px);
+    width: 50%;
+    height: 50%;
+    position: absolute;
+    top: ${(props) => props.t || "30%"};
+    left: ${(props) => props.l || "30%"};
+    z-index: 2;
+`;
 
 export const BoxHover = styled(motion.div)<BoxProps>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: ${props => props.backgroundImage || 'none'};
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  overflow: hidden;
-  z-index: 3;
-  background-origin: context-box;
-`
-
-export const BoxForPinkSpot = styled.div<SpotProps>`
-  position: absolute;
-  left: 30%;
-  width: 550px;
-  height: 500px;
-  background: linear-gradient(90deg, #C471F5 0%, #FA71CD 100%);
-  filter: blur(150px);
-  overflow: hidden;
-  transform: rotate(54.56deg);
-  overflow: hidden;
-
-  @media(max-width: 920px) {
-    height: 200px;
+    position: absolute;
     top: 0;
-    bottom: 50%;
     left: 0;
     right: 0;
-  }
-`
+    bottom: 0;
+    background: ${(props) => props.backgroundImage || "none"};
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    overflow: hidden;
+    z-index: 3;
+    background-origin: context-box;
+`;
+
+export const BoxForPinkSpot = styled.div<SpotProps>`
+    position: absolute;
+    left: 30%;
+    width: 550px;
+    height: 500px;
+    background: linear-gradient(90deg, #c471f5 0%, #fa71cd 100%);
+    filter: blur(150px);
+    overflow: hidden;
+    transform: rotate(54.56deg);
+    overflow: hidden;
+
+    @media (max-width: 920px) {
+        height: 200px;
+        top: 0;
+        bottom: 50%;
+        left: 0;
+        right: 0;
+    }
+`;
